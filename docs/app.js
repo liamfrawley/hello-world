6fa880f487b1088ab9f14482ee3106f3d935b7b0
Liam Frawley <liamfrawley@yahoo.com>
2018-11-27 09:59:30 +0000

    Update app.js

--- docs/app.js
+++ docs/app.js
@@ -2,7 +2,7 @@
 var graphApiEndpoint = "https://graph.microsoft.com/v1.0/me/drive/root/children";
  
 // Graph API scope used to obtain the access token to read user profile
-var graphAPIScopes = ["openid", "profile", "Files.ReadWrite.All", "Sites.Read.All", "Sites.ReadWrite.All", "offline_access"];
+var graphAPIScopes = ["Directory.Read.All", "Directory.ReadWrite.All", "Directory.AccessAsUser.All", "openid", "profile", "Files.ReadWrite.All", "Sites.Read.All", "Sites.ReadWrite.All", "offline_access"];
 //var graphAPIScopes = ["offline_access", "openid", "profile", "User.Read", "Files.ReadWrite"];
  
 // Initialize application
